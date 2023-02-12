52dfbe6c3168a77878ad928bdcae5273aa1f68d9
awasthilokesh2021 <115865786+awasthilokesh2021@users.noreply.github.com>
2023-02-12 22:20:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+let btn1 = document.getElementById("addTodoBtn")
+
+let btn = document.getElementById("todoList")
+
+let li1 = document.createElement("li")
+li1.textContent = "blue"
+
+let li2 = document.createElement("li")
+li2.textContent = "green"
+
+btn.appendChild(li1)
+btn.appendChild(li2)
 
