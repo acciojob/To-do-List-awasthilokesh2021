75688770b4ecf46e4bb0081cbd11df2ba9d9bc5d
awasthilokesh2021 <115865786+awasthilokesh2021@users.noreply.github.com>
2023-02-16 21:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 //your code here
-// let btn1 = document.getElementById("addTodoBtn")
-
-// let btn = document.getElementById("todoList")
-
-// let li1 = document.createElement("li")
-// li1.textContent = "blue"
-
-// let li2 = document.createElement("li")
-// li2.textContent = "green"
-
-// btn.appendChild(li1)
-// btn.appendChild(li2)
-
- 
-
-
-// let li2 = document.createElement("li")
-// li2.textContent = "green"
+var todovalue = document.getElementById("newTodoInput")
+ var btn = document.getElementById("addTodoBtn")
+var ol = document.getElementById('todoList')
 
 
 function addtodo(){
-    let btn1 = document.getElementById("todoList")
-    
-    let btn = document.createElement('li')
-    btn.textContent="blue"
 
-    btn1.appendChild(btn)
-    
+	if(todovalue.value){
+	   var li = document.createElement('li')
+		li.textContent = todovalue.value
+        
+        ol.appendChild('li')
+        todovalue.value = " "
+	}
        
 }
 
+btn.addEventListener('click',addtodo)
 
 
 
