5648620697f23ff89a99aba2582992ee1623d413
awasthilokesh2021 <115865786+awasthilokesh2021@users.noreply.github.com>
2023-02-16 21:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ var ol = document.getElementById('todoList')
 function addtodo(){
 
 	if(todovalue.value){
-	   var li = document.createElement('li')
+	   var li = document.createElement(li)
 		li.textContent = todovalue.value
         
         ol.appendChild('li')
-        todovalue.value = " "
+        todovalue.value = ""
 	}
        
 }
